8cec8a389a53906f30e00791f6f96015bf118f28
Carlos Bruno <cbcarlos08@gmail.com>
2017-09-28 07:53:03 -0400

    Botão voltar ao topo ativado

--- assets/js/email.js
+++ assets/js/email.js
@@ -154,3 +154,18 @@ $('.lnk-video').on('click', function () {
     console.log('Modal');
 });
 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('div[id="topo"]').fadeIn();
+        } else {
+            $('div[id="topo"]').fadeOut();
+        }
+    });
+
+    $('div[id="topo"]').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
+
